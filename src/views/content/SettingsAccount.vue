c8bcf9a333f181b8b3c55165d805670fcd3c3660
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2023-04-13 12:13:43 +0700

    make a change password

--- src/views/content/SettingsAccount.vue
+++ src/views/content/SettingsAccount.vue
@@ -8,15 +8,19 @@
                     </h6>
                 </div>
                 <div class="py-3 align-items-start">
-                    <Form @submit="changePassword">
+                    <Form @submit="changePassword" :validation-schema="schema" v-slot="{ errors }">
                         <div class="row">
                             <div class="col-sm-6 col-6">
                                 <label for="" class="mb-2">Password Baru</label>
-                                <InputField type="password" Name="newPass" v-model="newPass"  class="w-100"/>
+                                <InputField type="password" Name="newPass" :value="newPass" v-model="newPass"
+                                    class="w-100" />
+                                <span>{{ errors.newPass }}</span>
                             </div>
                             <div class="col-sm-6 col-6">
                                 <label for="" class="mb-2">Konfirmasi Password</label>
-                                <InputField type="password" Name="confirmPassword" v-model="confirmPassword" class="w-100" />
+                                <InputField type="password" Name="confirmPassword" :value="confirmPassword"
+                                    v-model="confirmPassword" class="w-100" />
+                                {{ errors.confirmPassword }}
                             </div>
                         </div>
                         <div class="d-flex justify-content-end">
@@ -31,43 +35,93 @@
 
 <script>
 import InputField from '@/components/InputField.vue';
-import {Form} from 'vee-validate';
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import iziToast from 'izitoast'
+import { Form } from 'vee-validate';
+import * as validate from 'yup'
 export default {
-    data(){
+    data() {
         return {
             newPass: '',
-            confirmPassword: '' 
+            confirmPassword: ''
         }
     },
     components: {
         InputField, Form,
     },
+    computed: {
+        schema() {
+            return validate.object({
+                newPass: validate.string().required('wajib diisi'),
+                confirmPassword: validate.string().required('wajib diisi'),
+            })
+        }
+    },
     mounted() {
         this.getUser()
     },
     methods: {
-        getUser(){
+        setPassword(event) {
+            this.newPass = event
+        },
+        setConfirmPassword(event) {
+            this.confirmPassword = event
+        },
+        logout() {
+            axios.get(
+                "https://berobatplus.shop/api/logout", {},
+                {
+                    headers: {
+                        Authorization: "Bearer" + Cookies.get('token')
+                    }
+                }
+            ).then(() => {
+                Cookies.remove('token')
+                Cookies.remove('user')
+                window.location.replace('/')
+            })
+        },
+        getUser() {
             let type = "getData"
             let url = [
                 "akun/profil/konsumen/profil", []
             ]
-            this.$store.dispatch(type, url).then((result)=>{
-            }).catch((err)=>{
+            this.$store.dispatch(type, url).then((result) => {
+            }).catch((err) => {
                 console.log(err);
             })
         },
-        changePassword(){
+        changePassword() {
             let type = "putData"
-            let url = [
-                "akun/change_password", {
-                    password_baru: this.newPass,
-                    konfirmasi_password: this.confirmPassword
+            if (this.newPass) {
+                if (this.confirmPassword !== this.newPass) {
+                    iziToast.error({
+                        title: "galat",
+                        message: "Password tidak sama",
+                        position: "topRight"
+                    })
+                } else {
+                    let url = [
+                        "akun/change_password", {
+                            password_baru: this.newPass,
+                            konfirmasi_password: this.confirmPassword
+                        }
+                    ]
+                    this.$store.dispatch(type, url).then((result) => {
+                        iziToast.success({
+                            title: "berhasil",
+                            message: "password diubah",
+                            position: "topRight",
+                            onOpened: () => {
+                            },
+                        })
+                        // this.logout()
+                    }).catch((err) => {
+                        console.log(err);
+                    })
                 }
-            ]
-            this.$store.dispatch(type, url).then((result)=>{
-            }).catch((err)=>{
-                console.log(err);
-            })
+            }
         }
     },
 }
