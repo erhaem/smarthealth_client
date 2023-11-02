520227ba820c81edacba525655bce68dce1378c1
Rifqi Haidar <jurig.blogger@gmail.com>
2023-11-02 12:59:17 +0700

    pindahan dari alvi, otp modal

--- src/views/auth/IndexRegister.vue
+++ src/views/auth/IndexRegister.vue
@@ -49,6 +49,101 @@
           </div>
         </div>
       </div>
+      <!-- MODAL OTP -->
+      <!-- <template> -->
+      <div
+        class="modal fade"
+        id="otpModal"
+        ref="otpModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+          <div class="modal-content">
+            <!-- <div class="modal-header"> -->
+
+            <!-- <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button> -->
+            <!-- </div> -->
+            <div class="modal-body">
+              <div class="container-fluid text-center p-4">
+                <h5 class="modal-title mb-4" id="exampleModalLabel">
+                  <b>Masukkan kode verifikasi</b>
+                </h5>
+                <p><b>Kode 6 digit verifikasi</b> telah dikirimkan melalui Nomor Whatsapp</p>
+                <div
+                  id="otp"
+                  class="inputs d-flex flex-row justify-content-center mt-2"
+                  style="height: 100px"
+                >
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="first"
+                    maxlength="1"
+                  />
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="second"
+                    maxlength="1"
+                  />
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="third"
+                    maxlength="1"
+                  />
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="fourth"
+                    maxlength="1"
+                  />
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="fifth"
+                    maxlength="1"
+                  />
+                  <input
+                    class="m-2 text-center form-control rounded"
+                    type="text"
+                    id="sixth"
+                    maxlength="1"
+                  />
+                </div>
+                <a href="#" class="text-decoration-none ms-3">Kirim Ulang(1/3)</a>
+              </div>
+              <div class="content d-flex justify-content-center align-items-center">
+                <span>Belum menerima kode ?</span>
+                <a href="#" class="text-decoration-none ms-2">Kirim Kode Melalui Email</a>
+              </div>
+              <div class="d-grid mx-auto mt-4 pb-4" style="width: 400px">
+                <button
+                  @click="handleSubmitOTP"
+                  class="btn btn-primary"
+                  style="height: 60px; border-radius: 25px"
+                >
+                  Verifikasi
+                </button>
+              </div>
+            </div>
+
+            <!-- <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <!-- </template> -->
+
       <div class="col-lg-6 d-none d-sm-block mt-5">
         <img src="../../assets/images/register.png" class="img-fluid" alt="" />
       </div>
@@ -73,7 +168,9 @@ export default {
         alamat: '',
         status: ''
       },
-      submitted: false
+      submitted: false,
+      otpCode: '',
+      user_id: ''
     }
   },
   components: {
@@ -87,6 +184,29 @@ export default {
     handleSubmit() {
       this.submitted = true
       let type = 'postData'
+
+      //TODO: validasi form
+
+      if (this.nik == '') {
+        this.$swal({
+          icon: 'error',
+          title: 'Failed!',
+          text: 'NIK masih kosong'
+        })
+
+        return
+      }
+
+      if (this.email == '') {
+        this.$swal({
+          icon: 'error',
+          title: 'Failed!',
+          text: 'Email masih kosong'
+        })
+
+        return
+      } // etc..
+
       const data = {
         nik: this.nik,
         nama: this.form.nama,
@@ -103,22 +223,114 @@ export default {
           if (result.success === false) {
             this.$swal({
               icon: 'error',
-              title: 'gagal',
-              text: 'Semua kolom wajib diisi'
+              title: 'Failed!',
+              text: Object.keys(result.data)
+                .map((err) => result.data[err])
+                .join(', ')
             })
           } else {
-            this.$swal({
-              icon: 'success',
-              title: 'berhasil register'
-            }).then(() => {
-              this.$router.push({ name: 'LoginUser' })
-            })
+            this.user_id = result.user
+
+            this.sendOTP()
+
+            // this.$swal({
+            //   icon: 'success',
+            //   title: 'berhasil register'
+            // }).then(() => {
+            //   this.$router.push({ name: 'LoginUser' })
+            // })
           }
         })
         .catch((err) => {
           console.log(err)
         })
+    },
+
+    verifyOTP() {
+      let type = 'postData'
+
+      const data = {
+        verification_code: this.otpCode,
+        user_id: this.user_id
+      }
+
+      let url = ['verify-otp-wa', data]
+
+      this.$store.dispatch(type, url).then((result) => {
+        if (result.success == false) {
+          this.$swal({
+            icon: 'error',
+            title: 'Gagal memverifikasi'
+          })
+          console.log('verifyotp: gagal, ', result.message)
+        } else {
+          console.log('verifyotp: sukses', result.message)
+          this.$swal({
+            icon: 'success',
+            title: 'Verifikasi sukses! Anda berhasil register'
+          }).then(() => {
+            this.$router.push({ name: 'LoginUser' })
+          })
+        }
+      })
+    },
+    sendOTP() {
+      let type = 'postData'
+
+      const data = {
+        to: this.form.nomor_hp,
+        user_id: this.user_id
+      }
+
+      let url = ['send-otp-wa', data]
+
+      this.$store.dispatch(type, url).then((result) => {
+        if (result.success == false) {
+          console.log('sendOTP: gagal, ', result.message)
+        } else {
+          console.log('sendOTP: sukses', result.message)
+          // this.$swal({
+          //   icon: 'success',
+          //   title: 'Success!',
+          //   text: 'Verifikasi berhasil! Akun anda berhasil terdaftar'
+          // })
+
+          //malah ga munculll
+          this.$nextTick(() => {
+            // pake function Bootstrap modal (jquery)
+            $(this.$refs.otpModal).modal('show')
+          })
+        }
+      })
     }
+  },
+
+  mounted() {
+    function OTPInput() {
+      const inputs = document.querySelectorAll('#otp > *[id]')
+      for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('keydown', function (event) {
+          if (event.key === 'Backspace') {
+            inputs[i].value = ''
+            if (i !== 0) inputs[i - 1].focus()
+          } else {
+            if (i === inputs.length - 1 && inputs[i].value !== '') {
+              return true
+            } else if (event.keyCode > 47 && event.keyCode < 58) {
+              inputs[i].value = event.key
+              if (i !== inputs.length - 1) inputs[i + 1].focus()
+              event.preventDefault()
+            } else if (event.keyCode > 64 && event.keyCode < 91) {
+              inputs[i].value = String.fromCharCode(event.keyCode)
+              if (i !== inputs.length - 1) inputs[i + 1].focus()
+              event.preventDefault()
+            }
+          }
+        })
+      }
+    }
+
+    OTPInput()
   }
 }
 </script>
