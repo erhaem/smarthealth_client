059208bd847ba3aff7e27cc04d34bd271a2f8f47
Rifqi Haidar <jurig.blogger@gmail.com>
2023-11-30 14:48:00 +0700

    chore(StartTest): ganti 'Semua Hasil' jadi 'Silakan pilih dokter'

--- src/views/content/test-stroke/StartTest.vue
+++ src/views/content/test-stroke/StartTest.vue
@@ -32,7 +32,7 @@
   </SectionHeaderBody>
   <div class="container mt-3">
     <div class="d-flex justify-content-between align-items-center mb-3">
-      <h6 class="mb-0"><b>Semua Hasil</b></h6>
+      <h6 class="mb-0"><b>Silakan pilih dokter</b></h6>
     </div>
 
     <!-- daftar dokter di bawah ini msih tumpang tindih - 22/11/23 -->
